e6dab5a176dbef280578a3014b2a9c02aaa96a01
Berker <68822081+berker00@users.noreply.github.com>
2023-06-14 19:39:34 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Yıllık.net - Ana Sayfa</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <header id="header" class="fixed-top header-transparent">
+    <div class="container d-flex align-items-center justify-content-between position-relative">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="index.html"><span>Yıllık.net</span></a></h1> 
+      </div>
+
+      <nav id="navbar" class="navbar d-none" style="color: #008bf8">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#">X</a></li>
+          <li><a class="nav-link scrollto" href="#">X</a></li>
+          <li><a class="nav-link scrollto" href="#">X</a></li>
+          <li><a class="nav-link scrollto" href="#">X</a></li>
+          <li class="dropdown"><a href="#"><span>X</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">X</a></li>             
+              <li><a href="#">X</a></li>
+              <li><a href="#">X</a></li>      
+            </ul>
+          </li>    
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+    </div>
+  </header>
+
+  <section id="ana">
+    <div class="ana">
+      <h1 class="ana-container" data-aos="fade-down">AtlasCO. Software</h1>
+      <h4 class="ana-container" data-aos="fade-down"><small>Andaç'ın Teknoloji ile Birleşimi</small></h4>
+      <h2 class="ana-container" data-aos="fade-down">Yapım Aşamasında...</h2>
+    </div>
+
+    <div class="hero">
+    
+      <div class="icon-boxes position-relative">
+        <div class="container position-relative">
+          <div class="row gy-2 mt-5" style="display: flex; justify-content: center;">
+  
+            <div class="col-xl-4 col-md-6 d-none" data-aos="fade-right" data-aos-delay="100">
+              <div class="icon-box">
+                <div class="icon"><i class="bi bi-person-fill-add"></i></div>
+                <h4 class="title"><a href="ogretmen_login.php" class="stretched-link">ÖĞRETMEN</a></h4>
+              </div>
+            </div>
+  
+            <div class="col-xl-4 col-md-6 d-none" data-aos="fade-left" data-aos-delay="200">
+              <div class="icon-box">
+                <div class="icon"><i class="bi bi-person-fill-check"></i></div>
+                <h4 class="title"><a href="ogrenci_login.php" class="stretched-link">ÖĞRENCİ</a></h4>
+              </div>
+            </div>
+  
+          </div>
+        </div>
+      </div>
+  
+      </div>
+    </div>
+
+  </section>
+
+  <main id="main">
+
+  <section id="about" class="about">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left">
+          <img src="assets/img/cta-bg.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
+          <h3>Arkadaşlarınla Yıllığı Paylaş: AtlasCO. Andaç</h3>
+          <p class="fst-italic">
+          Merhaba!
+          AtlasCO olarak, öğrencilerin unutulmaz anıları paylaşabilecekleri, interaktif bir yıllık yapımı sunuyoruz. 
+          Arkadaşlarınla birlikte özel anılarınızı kaydedebilir, fotoğraflarınızı paylaşabilir ve yıllığınızı online olarak oluşturabilirsiniz..
+          </p>
+          <ul>
+            <li><i class="bi bi-check-circle"></i> Çoklu Platform Desteği: AtlasCO. Andaç, masaüstü, mobil ve tablet cihazlarda sorunsuz bir şekilde çalışır. Böylece istediğiniz zaman ve herhangi bir cihazda projelerinize erişebilirsiniz.</li>
+            <li><i class="bi bi-check-circle"></i> Kolay kullanım: Platformumuz kullanıcı dostudur ve herkesin rahatlıkla kullanabileceği şekilde tasarlanmıştır.</li>
+          </ul>
+          <p>
+          AtlasCO. Andaç ile unutulmaz anılarınızı canlandırın ve arkadaşlarınızla eşsiz bir yıllık deneyimi yaşayın. Projenizi paylaşmak için sitemizi ziyaret edin ve bugünün anılarını yarının hatırası haline getirin!
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  
+  </main>
+
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row footer-position">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>İletişim</h3>
+              <p>           
+                <strong>Email:</strong> info@yillik.net<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="https://www.linkedin.com/in/berkerkeskin" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Bağlantılar</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Ana Sayfa</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Hizmetlerimiz</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Yapım Aşamasında</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container footer-bottom">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Yıllık.net</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="https://bekosec.dev">AtlasCO. Software</a>
+      </div>
+    </div>
+  </footer>
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader"></div>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
